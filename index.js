6c0933c96d75da09c47544513e88d70973f01156
Carlin Au <carlinau@gmail.com>
2017-04-26 06:51:12 -0400

    Added body parser

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res)=>{
   res.send('ayy lmaoo');
 });
 
 app.post('/', (req, res)=>{
-  console.log(req.body);
+  console.log(req.body.data);
   res.send('Request Received');
 });
 
